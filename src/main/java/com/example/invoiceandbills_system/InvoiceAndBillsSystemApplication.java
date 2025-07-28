e29ae6f5558d106c8538d8d604554910414ae331
Nada <148712191+nadamoustafa214@users.noreply.github.com>
2025-07-28 18:24:57 +0300

    first

---
+++ src/main/java/com/example/invoiceandbills_system/InvoiceAndBillsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoiceandbills_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceAndBillsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceAndBillsSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/invoiceandbills_system/InvoiceAndBillsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoiceandbills_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceAndBillsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

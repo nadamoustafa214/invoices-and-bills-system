8916c76d58db0b1ade8ab487305c05df73600a57
Nada <148712191+nadamoustafa214@users.noreply.github.com>
2025-11-12 19:04:57 +0200

    fix

--- src/main/java/com/example/invoiceandbills_system/api/v1/controller/ProductController.java
+++ src/main/java/com/example/invoiceandbills_system/api/v1/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.example.invoiceandbills_system.data.DTOs.ProductDto;
 import com.example.invoiceandbills_system.data.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,17 +14,19 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping
 public class ProductController implements ProductApi {
     private final ProductService productService;
 
 //@Override
-    public ResponseEntity<ProductDto> getProductById(Long id) {
-        if (id == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(productService.getProductById(id));
-    }
-//@Override
+//    public ResponseEntity<ProductDto> getProductById(Long id) {
+//        if (id == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(productService.getProductById(id));
+//    }
+
+    @GetMapping("/products")
     public ResponseEntity<List<ProductDto>> getProducts(){
         return ResponseEntity.ok(productService.getProducts());
     }
